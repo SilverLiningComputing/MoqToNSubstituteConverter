1d406ba9d7e1a89bd064aa29d4d1d675bcd3206f
Lisa Malenfant <lisa@silverliningcomputing.com>
2023-08-28 15:39:37 -0700

    Changed the namespace back to block

---
+++ MoqToNSubstitute.Tests/Helpers/Validation.cs
@@ -0,0 +1,37 @@
+﻿namespace MoqToNSubstitute.Tests.Helpers
+{
+    /// <summary>
+    /// Unit test validation methods
+    /// </summary>
+    public static class Validation
+    {
+        /// <summary>
+        /// Takes 2 objects of type T and makes sure the ValueTypes are equal
+        /// </summary>
+        /// <typeparam name="T">The type of object to compare</typeparam>
+        /// <param name="expected">The object with the expected values</param>
+        /// <param name="actual">The object with the actual values</param>
+        /// <returns>True if the objects are the same, false otherwise</returns>
+        public static bool Validate<T>(T expected, T actual) where T : class?
+        {
+            var properties = typeof(T).GetProperties();
+            foreach (var property in properties)
+            {
+                var expectedValue = property.GetValue(expected);
+                var actualValue = property.GetValue(actual);
+
+                if (expectedValue != null && !expectedValue.GetType().IsValueType)
+                {
+                    Validate(expectedValue, actualValue);
+                }
+                else
+                {
+                    Assert.AreEqual(expectedValue, actualValue);
+                }
+            }
+
+            return true;
+        }
+
+    }
+}
--- MoqToNSubstitute/Conversion/MoqToNSubstituteTransformer.cs
+++ MoqToNSubstitute/Conversion/MoqToNSubstituteTransformer.cs
@@ -7,53 +7,54 @@ using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("MoqToNSubstitute.Tests")]
 
-namespace MoqToNSubstitute.Conversion;
-
-internal class MoqToNSubstituteTransformer : ICodeTransformer
+namespace MoqToNSubstitute.Conversion
 {
-    public void Transform(string sourceFilePath, bool transform = false)
+    internal class MoqToNSubstituteTransformer : ICodeTransformer
     {
-        var substitutions = ReplacementTemplate.ReturnReplacementSyntax();
-        var sourceText = File.ReadAllText(sourceFilePath);
-
-        if (!sourceText.Contains("Mock")) return;
-
-        var tree = CSharpSyntaxTree.ParseText(sourceText);
-        var root = tree.GetRoot();
-
-        var rootObject = root.ReplaceArgumentNodes(substitutions, ".Object", "It.IsAny", "It.Is");
-        var rootAssignment = rootObject.ReplaceAssignmentNodes(substitutions, "Mock");
-        var rootNewObject = rootAssignment.ReplaceObjectCreationNodes(substitutions, "new Mock");
-        var rootFields = rootNewObject.ReplaceVariableNodes(substitutions, "Mock<");
-        var rootExpression = rootFields.ReplaceExpressionNodes(substitutions, ".Setup(", ".Verify(");
-
-        Logger.Log($"Modified File: \r\n{rootExpression}\r\n");
-        var modifiedCode = rootExpression.NormalizeWhitespace().ToFullString();
-        if (root.ToFullString() == rootExpression.ToFullString() || !transform) return;
-        File.WriteAllText(sourceFilePath, modifiedCode);
-    }
-
-    internal static void GetNodeTypesFromFile(string sourceFilePath)
-    {
-        var sourceText = File.ReadAllText(sourceFilePath);
-        if (!sourceText.Contains("Mock")) return;
-        GetNodeTypesFromString(sourceText);
-    }
-
-    internal static void GetNodeTypesFromString(string sourceText)
-    {
-        var tree = CSharpSyntaxTree.ParseText(sourceText);
-        var root = tree.GetRoot();
-
-        var descendantNodes = root.DescendantNodes();
-        var descendantNodeArray = descendantNodes.ToArray();
-
-        foreach (var node in descendantNodeArray)
+        public void Transform(string sourceFilePath, bool transform = false)
         {
-            var originalCode = node.ToString();
-            var nodeType = node.GetType().ToString();
+            var substitutions = ReplacementTemplate.ReturnReplacementSyntax();
+            var sourceText = File.ReadAllText(sourceFilePath);
 
-            Logger.Log($"Line: {node.GetLocation().GetLineSpan().StartLinePosition.Line}, Original: {originalCode}, Node Type: {nodeType}");
+            if (!sourceText.Contains("Mock")) return;
+
+            var tree = CSharpSyntaxTree.ParseText(sourceText);
+            var root = tree.GetRoot();
+
+            var rootObject = root.ReplaceArgumentNodes(substitutions, ".Object", "It.IsAny", "It.Is");
+            var rootAssignment = rootObject.ReplaceAssignmentNodes(substitutions, "Mock");
+            var rootNewObject = rootAssignment.ReplaceObjectCreationNodes(substitutions, "new Mock");
+            var rootFields = rootNewObject.ReplaceVariableNodes(substitutions, "Mock<");
+            var rootExpression = rootFields.ReplaceExpressionNodes(substitutions, ".Setup(", ".Verify(");
+
+            Logger.Log($"Modified File: \r\n{rootExpression}\r\n");
+            var modifiedCode = rootExpression.NormalizeWhitespace().ToFullString();
+            if (root.ToFullString() == rootExpression.ToFullString() || !transform) return;
+            File.WriteAllText(sourceFilePath, modifiedCode);
+        }
+
+        internal static void GetNodeTypesFromFile(string sourceFilePath)
+        {
+            var sourceText = File.ReadAllText(sourceFilePath);
+            if (!sourceText.Contains("Mock")) return;
+            GetNodeTypesFromString(sourceText);
+        }
+
+        internal static void GetNodeTypesFromString(string sourceText)
+        {
+            var tree = CSharpSyntaxTree.ParseText(sourceText);
+            var root = tree.GetRoot();
+
+            var descendantNodes = root.DescendantNodes();
+            var descendantNodeArray = descendantNodes.ToArray();
+
+            foreach (var node in descendantNodeArray)
+            {
+                var originalCode = node.ToString();
+                var nodeType = node.GetType().ToString();
+
+                Logger.Log($"Line: {node.GetLocation().GetLineSpan().StartLinePosition.Line}, Original: {originalCode}, Node Type: {nodeType}");
+            }
         }
     }
 }
